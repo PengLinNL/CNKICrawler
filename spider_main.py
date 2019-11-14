9bc8baa88e9b542ecb842305c36d9c734a043299
50000code <55131323+50000code@users.noreply.github.com>
2019-11-14 10:18:16 +0800

    111

--- spider_main.py
+++ spider_main.py
@@ -1,74 +1,57 @@
-# -*- coding: utf-8 -*-
-from configparser import ConfigParser
-from urllib.parse import quote
-import socket
-import os
-import math
-import urllib.request
+# -*- coding:utf-8 -*-
+import sys
+import requests
 from bs4 import BeautifulSoup
+import math
+import io
 import time
-import spider_search_page
-import spider_paper
+import os
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
 
 if __name__ == '__main__':
-    start = time.clock()
-    cf = ConfigParser()
-    cf.read("Config.conf", encoding='utf-8')
-    keyword = cf.get('base', 'keyword')# 关键词
-    maxpage = cf.getint('base', 'maxpage')# 最大页码
-    searchlocation = cf.get('base', 'searchlocation') #搜索位置
-    currentpage = cf.getint('base', 'currentpage')
-    if os.path.exists('data-detail.txt') and currentpage == 0:
-        print('存在输出文件，删除该文件')
+    #获取开始时间
+    start=time.perf_counter()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
+    }
+    # 避免之前的内容重复爬取
+    if os.path.exists('data-detail.txt'):
+        print("存在输出文件，删除文件")
         os.remove('data-detail.txt')
 
-    #构造不同条件的关键词搜索
-    values = {
-           '全文': 'qw',
-           '主题': 'theme',
-           '篇名': 'title',
-           '作者': 'author',
-           '摘要':'abstract'
-    }
-    keywordval = str(values[searchlocation])+':'+str(keyword)
-    index_url='http://search.cnki.com.cn/Search.aspx?q='+quote(keywordval)+'&rank=&cluster=&val=&p='#quote方法把汉字转换为encodeuri?
-    print(index_url)
+# 获取页数，可以根据搜索关键词进行url修改，这里是“大数据”
+index_url = r'http://search.cnki.com.cn/Search.aspx?q=大数据&rank=relevant&cluster=zyk&val=CJFDTOTAL'
+htm1 = requests.get(index_url,headers=headers)
+soup = BeautifulSoup(htm1.text, 'html.parser')
+pagesum_text = soup.find('span', class_='page-sum').get_text()
+maxpage = math.ceil(int(pagesum_text[7:-1]) / 15)
+print('The total page is:', maxpage)
 
-    #获取最大页数
-    html = urllib.request.urlopen(index_url).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    pagesum_text = soup.find('span', class_='page-sum').get_text()
-    maxpage = math.ceil(int(pagesum_text[7:-1]) / 15)
-    #print(maxpage)
-    cf = ConfigParser()
-    cf.read("Config.conf", encoding='utf-8')
-    cf.set('base', 'maxpage', str(maxpage))
-    cf.write(open('Config.conf', 'w', encoding='utf-8'))
+# 获取各检索结果文章链接
+for i in range(0, maxpage):
+    page_num = 15  # 一页共有15条搜索结果
+    url = index_url + '&p=' + str(i * page_num)  # 构建url链接
 
-    for i in range(currentpage, maxpage):
-        page_num=15
-        page_str_num=i*page_num
-        page_url=index_url+str(page_str_num)
-        print(page_url)
-        attempts = 0
-        success = False
-        while attempts < 50 and not success:
-            try:
-                spider_search_page.get_paper_url(page_url)
-                socket.setdefaulttimeout(10)  # 设置10秒后连接超时
-                success = True
-            except socket.error:
-                attempts += 1
-                print("第"+str(attempts)+"次重试！！")
-                if attempts == 50:
-                    break
-            except urllib.error:
-                attempts += 1
-                print("第"+str(attempts)+"次重试！！")
-                if attempts == 50:
-                    break
-        cf.set('base', 'currentpage', str(i))
-        cf.write(open("Config.conf", "w", encoding='utf-8'))
-    spider_paper.spider_paper()# spider_paper补全文章信息
-    end = time.clock()
-    print ('Running time: %s Seconds'%(end-start))
+    try:
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+    except:
+        break
+
+    f = open('data-detail.txt', 'a+', encoding='utf-8')
+    all = soup.find_all('div', class_='wz_content')
+    for string in all:
+        item = string.find('a', target='_blank')  # 文章标题与链接
+        href = item.get('href')  # 获取文章链接
+        # title=item.get_text()#获取文章标题
+        f.write(href + '\n')
+
+    f.close()
+
+    #获取结束时间
+    end=time.clock()
+    print('获取文章详情页链接共用时：%s Seconds'%(end-start))
+
+
+
--- spider_paper.py
+++ spider_paper.py
@@ -1,149 +1,90 @@
-# -*- coding: utf-8 -*-
-import socket
-
-from bs4 import BeautifulSoup
-import urllib
 import requests
-import time
+from bs4 import BeautifulSoup
 import xlwt
-from configparser import ConfigParser
+import re
+import time
 
-def spider_paper():
-    start = time.clock()
-    # f=urllib2.urlopen(url, timeout=5).read()
-    # soup=BeautifulSoup(html)
-    # tags=soup.find_all('a')
-    file = open("data-detail.txt", encoding='utf8')
-    cf = ConfigParser()
-    cf.read("Config.conf", encoding='utf-8')
-    keyword = cf.get('base', 'keyword')# 关键词
+headers = {
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding':'gzip, deflate, sdch',
+    'Accept-Language':'zh-CN,zh;q=0.8',
+    'Connection':'keep-alive',
+    'Host':'www.cnki.net',
+    'Referer':'http://search.cnki.net/search.aspx?q=%E4%BD%9C%E8%80%85%E5%8D%95%E4%BD%8D%3a%E6%AD%A6%E6%B1%89%E5%A4%A7%E5%AD%A6&rank=relevant&cluster=zyk&val=CDFDTOTAL',
+    'Upgrade-Insecure-Requests':'1',
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+}
 
-# 写入Excel
-    wb = xlwt.Workbook("data_out.xls")
-    sheet = wb.add_sheet("data-out")
-    sheet.write(0, 0, '下载网址')
-    sheet.write(0, 1, '标题')
-    sheet.write(0, 2, '来源')
-    sheet.write(0, 3, '引用')
-    sheet.write(0, 4, '作者')
-    sheet.write(0, 5, '作者单位')
-    sheet.write(0, 6, '关键词')
-    sheet.write(0, 7, '摘要')
-    sheet.write(0, 8, '共引文献')
+if __name__=='__main__':
+    start=time.process_time()
+#读取已经获得的文章链接
+file=open('data-detail.txt',encoding="utf-8")
 
-    lines = file.readlines()
-    txt_num = 1
-    lin_num = 1
-    paper_list = []
-    for line in lines:
-        object = line.split('\t')
-        paper_url = object[0]
-        if paper_url in paper_list:
-            continue
-        paper_list.append(paper_url)
-        attempts = 0
-        success = False
-        while attempts < 50 and not success:
-            try:
-                html = urllib.request.urlopen(paper_url).read()
-                soup = BeautifulSoup(html, 'html.parser')
-                socket.setdefaulttimeout(10)  # 设置10秒后连接超时
-                success = True
-            except socket.error:
-                attempts += 1
-                print("第"+str(attempts)+"次重试！！")
-                if attempts == 50:
-                    break
-            except urllib.error:
-                attempts += 1
-                print("第"+str(attempts)+"次重试！！")
-                if attempts == 50:
-                    break
-        title = soup.find_all('div', style="text-align:center; width:740px; font-size: 28px;color: #0000a0; font-weight:bold; font-family:'宋体';")
-        abstract = soup.find_all('div', style='text-align:left;word-break:break-all')
-        author = soup.find_all('div', style='text-align:center; width:740px; height:30px;')
+#xls创建与写入
+wb=xlwt.Workbook('data_out.xsl')
+sheet=wb.add_sheet('data-out')
+sheet.write(0,0,'网址')
+sheet.write(0,1,'标题')
+sheet.write(0,2,'作者')
+sheet.write(0,3,'机构')
+sheet.write(0,4,'摘要')
+sheet.write(0,5,'关键词')
 
-        #获取作者名字
-        for item in author:
-            author = item.get_text()
-        # print(item)
-        #获取摘要信息
-        tmp = ''
-        for thing in abstract:
-            a = thing.strings
-            for string in a:
-                tmp = tmp + string
-            txt_num += 1
-        result = tmp.split(' ')
-        tstr = ''
-        for t in result:
-            test = t.split('\n')
-            # print(test)
-            if test != '\t' and test != '\n' and test != '\r' and test != '':
-                for i in test:
-                    if len(i) > 1:
-                        item = i.split('\r')
-                        for j in item:
-                            object = j.split('\t')
-                            for k in object:
-                                tstr += k
+lin_num=1
+txt_num=1
 
-        ifreferen = soup.find_all('td', class_='b14', rowspan='2')
-        ref = ''
-        for i in range(len(ifreferen)):
-            if ('【共引文献】' in ifreferen[i].get_text()):
-                referenceList = soup.find_all('div', id='div_Ref')  # 共引文献列表
-                if len(referenceList) == 0:
-                    referenceList = soup.find_all('div', class_='div_Ref')
-                referenceList = referenceList[i]
-                for tdref in referenceList.find_all('td', width='676'):
-                    refitem = tdref.a.get("href")
-                    refitem = refitem.strip()
-                    print(refitem)
-                    ref = ref + refitem + ' ,'
-        # 获取作者单位，处理字符串匹配
-        authorUnitScope = soup.find('div', style='text-align:left;', class_='xx_font')
-        author_unit = ''
-        author_unit_text = authorUnitScope.get_text()
-        # print(author_unit_text)
-        if '【作者单位】：' in author_unit_text:
-            auindex = author_unit_text.find('【作者单位】：', 0)
-        else:
-            auindex = author_unit_text.find('【学位授予单位】：', 0)
-        for k in range(auindex, len(author_unit_text)):
-            if author_unit_text[k] == '\n' or author_unit_text[k] == '\t' or author_unit_text[k] == '\r' or \
-                        author_unit_text[k] == '】':
-                continue
-            if author_unit_text[k] == ' ' and author_unit_text[k + 1] == ' ':
-                continue
-            if author_unit_text[k] != '【':
-                author_unit = author_unit + author_unit_text[k]
-            if author_unit_text[k] == '【' and k != auindex:
-                break
-        # 获取关键字
-        key_word = ''
-        kwindex = author_unit_text.find('【关键词】：', 0)
-        for k in range(kwindex, len(author_unit_text)):
-            if author_unit_text[k] == '\n' or author_unit_text[k] == '\t' or author_unit_text[k] == '\r' or \
-                                author_unit_text[k] == '】':
-                continue
-            if author_unit_text[k] == ' ' and author_unit_text[k + 1] == ' ':
-                continue
-            if author_unit_text[k] != '【':
-                key_word = key_word + author_unit_text[k]
-            if author_unit_text[k] == '【' and k != kwindex:
-                break
-        # print(author_unit)
-        # print(key_word)
-        line = line.strip('\n')
-        line = line + '\t' + str(author) + '\t' + str(author_unit) + '\t'+ str(key_word) + '\t'+ str(tstr) + '\t' + str(ref) + '\n'
-        outstring = line.split('\t')
+for href in file:
+    if  re.match(r'^(http://www.cnki.com.cn/Article/CJFDTOTAL)-\w{4}(\w*)',href):
+        year=href[-14:-10]
+        name=href[-18:-5]
+        #含关键词的详情页链接
+        paper_url= "http://kns.cnki.net/KCMS/detail/detail.aspx?dbcode=CJFQ&dbname=CJFDLAST" + year + "&filename=" +name
+        try:
+
+            html = requests.get(paper_url, headers=headers, timeout=500)
+            soup = BeautifulSoup(html.text, 'html.parser')
+
+        except:
+            print("No result")
+            break
+
+        # 获取标题
+        title = soup.find('h2', class_='title').get_text()
+
+        # 获取作者
+       # author = soup.find('div', class_='author').get_text() 比较简单，但是作者姓名容易黏在一起
+        author=''
+        for a in soup.find('div', class_='author').children:
+            author+=(a.get_text()+';')
+
+        # 获取机构
+        orgn = soup.find('div', class_='orgn').get_text()
+
+        # 获取摘要
+        abstract = soup.find('span', id='ChDivSummary').get_text()
+
+        #获取关键词，存在没有关键词的情况
+        try:
+            key = ''
+            for k in soup.find('label', id='catalog_KEYWORD').next_siblings:
+              ke = (k.get_text()).strip()
+              key += ke.replace('\r\n', '')
+        except:
+          pass
+
+        print(title)
+
+        line=paper_url+'\t'+str(title)+'\t'+str(author)+'\t'+str(orgn)+'\t'+str(abstract)+'\t'+str(key)+'\n'
+        outstring=line.split('\t')
         for i in range(len(outstring)):
-            sheet.write(lin_num, i, outstring[i])
+            #写入
+           sheet.write(lin_num,i,outstring[i])
         print('写入第'+str(lin_num)+'行')
-        lin_num += 1
-        wb.save('data_out_'+str(keyword)+'.xls')
+        lin_num+=1
 
-    file.close()
-    end = time.clock()
-    print('Running time: %s Seconds' % (end - start))
+        #保存成xx文件
+        wb.save('data_out_'+str('大数据'+'.xls'))
+
+file.close()
+end=time.process_time()
+print('完成论文数据获取共用时：%s Seconds'%(end-start))
